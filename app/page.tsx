83271aa69c115665100eaedfb873dd40e251168b
Samuel <samueljuliank29@gmail.com>
2026-02-24 20:09:22 +0800

    Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,90 @@
-import React from 'react'
+import DataTable from "@/components/DataTable";
+import { cn } from "@/lib/utils";
+import { TrendingDown, TrendingUp } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+const columns: DataTableColumn<TrendingCoin>[] = [
+  {
+    header: "Name",
+    cellClassName: "name-cell",
+    cell: (coin) => {
+      const item = coin.item;
+
+      return (
+        <Link href={`/coins/${item.id}`}>
+          <Image src={item.large} alt="item.name" width={36} height={36} />
+          <p>{item.name}</p>
+        </Link>
+      );
+    },
+  },
+
+  {
+    header: "24h Change",
+    cellClassName: "name-cell",
+    cell: (coin) => {
+      const item = coin.item;
+      const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
+
+      return (
+        <div
+          className={cn(
+            "price-change",
+            isTrendingUp ? "text-green-500" : "text-red-500",
+          )}
+        >
+          <p>
+            {isTrendingUp ? (
+              <TrendingUp width={16} height={16} />
+            ) : (
+              <TrendingDown width={16} height={16} />
+            )}
+          </p>
+        </div>
+      );
+    },
+  },
+
+  {
+    header: "Price",
+    cellClassName: "price-cell",
+    cell: (coin) => coin.item.data.price,
+  },
+];
 
 const page = () => {
-  return <div></div>
-}
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <div id="coin-overview">
+          <div className="header pt-2 ">
+            <Image
+              src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png"
+              alt="Bitcoin"
+              width={56}
+              height={56}
+            />
 
-export default page
+            <div className="info">
+              <p>Bitcoin / BTC </p>
+              <h1>$86,113.00</h1>
+            </div>
+          </div>
+        </div>
+
+        <p>Trending Coins</p>
+        <DataTable
+          data={[]}
+          columns={[{ header: "Title" }, { header: "Price" }]}
+        />
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
+};
+
+export default page;
